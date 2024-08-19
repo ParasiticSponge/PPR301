53e67a91f954a9ecbb67a0011ececd8abb56df72
Samantha <123sjstevens@gmail.com>
2024-08-19 16:03:35 +1000

    cut door into maze glass and polished wheel. Also added tut room

--- PPR301/Intermediate/Build/BuildRulesProjects/PPR301ModuleRules/obj/Development/PPR301ModuleRules.AssemblyInfo.cs
+++ PPR301/Intermediate/Build/BuildRulesProjects/PPR301ModuleRules/obj/Development/PPR301ModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PPR301ModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+704db5b65dfc0706ed5fdc1b5001fa7bf6b99501")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15886cdf126c5fbae117fb7a054f20ddefc6dae0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PPR301ModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PPR301ModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
