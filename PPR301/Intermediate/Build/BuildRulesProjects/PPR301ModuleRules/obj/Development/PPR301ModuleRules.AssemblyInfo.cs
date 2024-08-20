429805f8271db48fe4aab20f85f48b9580824fde
Samantha <123sjstevens@gmail.com>
2024-08-20 10:25:19 +1000

    tutorial room edit and other maze stuff

--- PPR301/Intermediate/Build/BuildRulesProjects/PPR301ModuleRules/obj/Development/PPR301ModuleRules.AssemblyInfo.cs
+++ PPR301/Intermediate/Build/BuildRulesProjects/PPR301ModuleRules/obj/Development/PPR301ModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PPR301ModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53e67a91f954a9ecbb67a0011ececd8abb56df72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6402f7b0879cb4c18c1cc16abd78a2d2aef56963")]
 [assembly: System.Reflection.AssemblyProductAttribute("PPR301ModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PPR301ModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
