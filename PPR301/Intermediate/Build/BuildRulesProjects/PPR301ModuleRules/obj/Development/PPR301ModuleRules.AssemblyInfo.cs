f4069a2beaf09e68e1e226eb5c612d6e98398f1d
Samantha <123sjstevens@gmail.com>
2024-08-20 15:01:51 +1000

    moving walls and material edit

--- PPR301/Intermediate/Build/BuildRulesProjects/PPR301ModuleRules/obj/Development/PPR301ModuleRules.AssemblyInfo.cs
+++ PPR301/Intermediate/Build/BuildRulesProjects/PPR301ModuleRules/obj/Development/PPR301ModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PPR301ModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6402f7b0879cb4c18c1cc16abd78a2d2aef56963")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e54013aa6a3606e90a7b264d818ac0b1539aa0c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PPR301ModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PPR301ModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
